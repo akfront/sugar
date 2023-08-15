e921aefec17d2501a2389511637a58ebb8e20810
khlestkinaleksey <khlestkinvarshaveraleksey@gmail.com>
2023-08-16 01:16:15 +0300

    Initial Commit

---
+++ nosugar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nosugar
+//
+//  Created by 1234 on 16.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
